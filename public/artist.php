d95c209e62a85382993e06c2838f62bb3b8f97d5
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-05-13 18:53:44 +0200

    Commit fin de scéance

--- public/artist.php
+++ public/artist.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 use Database\MyPdo;
 use Html\WebPage;
 
-$artistId = 17;
+$artistId = $_GET['artistId'];
 
+$web = new WebPage();
+$web->getTitle('Saisir ID');
 
 MyPDO::setConfigurationFromIniFile();
 $art = MyPDO::getInstance()->prepare(
@@ -18,10 +20,14 @@ SQL
 $art->bindParam(':id', $artistId, PDO::PARAM_INT);
 $art->execute();
 
+
 $web = new WebPage();
 
 while (($ligne = $art->fetch()) !== false) {
     $res = WebPage::escapeString($ligne['name']);
+}
+if ($artistId == null || $artistId > $res){
+    header('Location: http://localhost:8000/');
 }
 $web->setTitle('Albums de '.$res);
 $web->appendContent("<h1>Albums de ".$res."</h1>");
@@ -30,7 +36,7 @@ $web->appendContent("<h1>Albums de ".$res."</h1>");
 
 $album = MyPDO::getInstance()->prepare(
     <<<'SQL'
-    SELECT al.name
+    SELECT al.name, al.year
     FROM album al INNER JOIN artist ar ON (al.artistId=ar.id)
     WHERE ar.id = :id
 SQL
@@ -40,7 +46,8 @@ $album->execute();
 
 while (($idAlb = $album->fetch()) !== false) {
     $nomAlb = WebPage::escapeString($idAlb['name']);
-    $web->appendContent("<p>$nomAlb</p>\n");
+    $dateAlb = WebPage::escapeString($idAlb['year']);
+    $web->appendContent("<p>$dateAlb $nomAlb</p>\n");
 }
 
 echo $web->toHTML();
