154c66db841655f72440acc0bc4f7f5d0bf45af1
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-05-17 18:53:18 +0200

    git fin de scéance

---
+++ src/Entity/Collection/ArtistCollection.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Entity\Artist;
+
+class ArtistCollection
+{
+    /**Search a database collection for a document or set of documents.
+     * The found documents are returned as a CollectionFind object is
+     * to further modify or fetch results from.
+     * @return Artist[]
+     */
+    public static function findAll(): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+        SELECT name
+        FROM artist
+        ORDER BY name
+        SQL
+        );
+
+        $stmt->execute();
+        $res = $stmt->fetchAll(PDO::FETCH_CLASS||PDO::FETCH_PROPS_LATE);
+        return $res;
+    }
+}
