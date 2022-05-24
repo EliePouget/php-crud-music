add1303c273be4d6e98ae87038c39cf4a9326321
Elie Pouget <elie.pouget@etudiant.univ-reims.fr>
2022-05-24 18:28:42 +0200

    Commit fin de TP2

--- public/artist.php
+++ public/artist.php
@@ -1,54 +1,25 @@
 <?php
 
 declare(strict_types=1);
+use Entity\Album;
 use Database\MyPdo;
 use Html\WebPage;
 
-$artistId = $_GET['artistId'];
+$artistId = intval($_GET['artistId']);
 
 $web = new WebPage();
-$web->getTitle('Saisir ID');
+$artist = new \Entity\Artist();
+$artist = $artist->findById($artistId);
 
-MyPDO::setConfigurationFromIniFile();
-$art = MyPDO::getInstance()->prepare(
-    <<<'SQL'
-    SELECT name
-    FROM artist
-    WHERE id = :id
-SQL
-);
-$art->bindParam(':id', $artistId, PDO::PARAM_INT);
-$art->execute();
+$album = new \Entity\Collection\AlbumCollection();
+$stmt = $album->findByArtistId($artistId);
 
+$web->setTitle("Album de " . $artist->getName());
 
-$web = new WebPage();
-
-while (($ligne = $art->fetch()) !== false) {
-    $res = WebPage::escapeString($ligne['name']);
-}
-if ($artistId > $res || $artistId < 0) {
-    header('Location: http://localhost:8000/');
-}
-
-$web->setTitle('Albums de '.$res);
-$web->appendContent("<h1>Albums de ".$res."</h1>");
-
-
-
-$album = MyPDO::getInstance()->prepare(
-    <<<'SQL'
-    SELECT al.name, al.year
-    FROM album al INNER JOIN artist ar ON (al.artistId=ar.id)
-    WHERE ar.id = :id
-SQL
-);
-$album->bindParam(':id', $artistId, PDO::PARAM_INT);
-$album->execute();
-
-while (($idAlb = $album->fetch()) !== false) {
-    $nomAlb = WebPage::escapeString($idAlb['name']);
-    $dateAlb = WebPage::escapeString($idAlb['year']);
-    $web->appendContent("<p>$dateAlb $nomAlb</p>\n");
+for ($i = 0; $i<count($stmt);$i++) {
+    $year = WebPage::escapeString((string)$stmt[$i]->getYear());
+    $albumName = WebPage::escapeString($stmt[$i]->getName());
+    $web->appendContent("<p>$year $albumName</p>");
 }
 
 echo $web->toHTML();
--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace Entity;
 
 use Database\MyPdo;
+use Entity\Collection\AlbumCollection;
 use Entity\Exception\EntityNotFoundException;
+use PDO;
+use Psr\Container\NotFoundExceptionInterface;
+use function PHPUnit\Framework\throwException;
 
 class Artist
 {
@@ -28,27 +32,38 @@ class Artist
         return $this->name;
     }
 
-    public static function findById(int $id): \PDOStatement
+    public static function findById(int $id): Artist
     {
-        $artist = MyPDO::getInstance()->prepare(<<<'SQL'
-        SELECT name
-        FROM artist
-        WHERE id = :id
-        SQL);
-        $artist->bindParam(':id', $id);
-        $artist->setFetchMode(PDO::FETCH_CLASS| PDO::FETCH_PROPS_LATE);
-        $artist->execute([":Artist"=> $id]);
-        $artist = $artist->fetchAll();
-        if ($artist == []) {
-            throw new EntityNotFoundException("L'entité est introuvable");
+        $art = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT name, id
+            FROM artist
+            WHERE id = :id
+        SQL
+        );
+        $art->bindParam(':id', $id, PDO::PARAM_INT);
+        $art->execute();
+        $artist = $art->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, "Entity\Artist");
+        if (!$artist) {
+            throw new EntityNotFoundException("L'id de l'album est incorrect");
         }
         return $artist[0];
     }
 
-
-    public function getAlbums(): Album
+    public function getAlbums(): array
     {
-        $res = findByArtistId($this->getId());
+        $list_id = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id 
+            FROM artist
+            SQL
+        );
+        $list_id->execute();
+        $list_id->fetchAll();
+        $res = [];
+        for ($i=0;$i<count($list_id);$i++) {
+            $res[] = AlbumCollection::findByArtistId($list_id[$i]);
+        }
         return $res;
     }
 }
